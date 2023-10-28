965e2e9f4ff7e423487a841a1b7d8e2cc1d9e311
Sonu Kumar Singh <hi.ersonusingh@gmail.com>
2023-10-28 12:46:09 +0530

    index.php

--- index.php
+++ index.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <title>Ecommerce Project</title>
 </head>
 <body>
-    
+    <div class="row">
+        <div class="col-5 bg-primary">
+            <h1>This is part1</h1>
+        </div>
+        <div class="col-4 bg-success">
+            <h1>This is part2</h1>
+        </div>
+        <div class="col-3 bg-secondary">
+            <h1>This is part3</h1>
+        </div>
+    </div>
 </body>
 </html>
 
